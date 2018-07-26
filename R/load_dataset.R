063ae0ae6f4a894f32fed2d54c43159ee04060b4
anne <amf42002@yahoo.com>
2018-07-26 12:46:58 -0500

    - add code to load_dataset.R - clean up empdata related dataframe names to use the '.' convention, i.e. read data into empdata.raw dataframe

--- R/load_dataset.R
+++ R/load_dataset.R
@@ -1,7 +1,16 @@
 ####################################
 # Script name : load_dataset.R
-# Created By  :
-# Created Dt  :
-# Description :
+# Created By  : Anne Francomano
+# Created Dt  : 07/26/2018
+# Description : R code to load and clean the employee data provided by Qualtrics
+# Source File : This script uses the source file EmployeeData201807.csv 
 ###################################
-  
+##@knitr load_dataset
+
+# Note manual preparation steps for excel workbook data prior to loading into R:
+#     -- Copy excel sheet to a new excel workbook
+#     -- Delete the two extra worksheets that are created by default
+#     -- Save the excel file as an MSDOS CSV file with the name EmployeeData201807.csv
+
+# Load the state size file data into R
+empdata.raw <- read.csv(file="Data/EmployeeData201807.csv", header=TRUE, sep=",")
--- R/tidy_dataset.R
+++ R/tidy_dataset.R
@@ -1,82 +1,83 @@
 ###########################################################################################
-## R code to load and clean the employee data provided by Qualtrics 
-## Author : Anne Francomano
-## Created Date : 07/22/2018
-## This script uses the source file EmployeeData201807.csv 
+# Script name : tidy_dataset.R 
+# Created By  : Anne Francomano
+# Created Dt  : 07/22/2018
+# Description : R code clean the employee data provided by Qualtrics
+# Source File : This script uses the source file EmployeeData201807.csv
 ###########################################################################################
 ##@knitr tidy_dataset
-# Note manual preparation steps for excel workbook data prior to loading into R:
-#     -- Copy excel sheet to a new excel workbook
-#     -- Delete the two extra worksheets that are created by default
-#     -- Save the excel file as an MSDOS CSV file
-# Load the state size file data into R
-empdata <- read.csv(file="Data/EmployeeData201807.csv", header=TRUE, sep=",")
+
 # Verify number of rows and columns in empdata dataframe
-dim(empdata)
+dim(empdata.raw)
 # Verify the structure of empdata dataframe
-str(empdata)
+str(empdata.raw)
 # Reviewed each data colum and determined that multiple fields need to be converted from INT class to FACTOR class
 #     Class of Education field should be FACTOR not INT
-empdata[,'Education']<-factor(empdata[,'Education'])
+empdata.raw[,'Education']<-factor(empdata.raw[,'Education'])
 
 #     Class of EnvironmentSatisfaction field should be FACTOR not INT
-empdata[,'EnvironmentSatisfaction']<-factor(empdata[,'EnvironmentSatisfaction'])
+empdata.raw[,'EnvironmentSatisfaction']<-factor(empdata.raw[,'EnvironmentSatisfaction'])
 
 #     Class of JobInvolvement field should be FACTOR not INT
-empdata[,'JobInvolvement']<-factor(empdata[,'JobInvolvement'])
+empdata.raw[,'JobInvolvement']<-factor(empdata.raw[,'JobInvolvement'])
 
 #     Class of JobLevel field should be FACTOR not INT
-empdata[,'JobLevel']<-factor(empdata[,'JobLevel'])
+empdata.raw[,'JobLevel']<-factor(empdata.raw[,'JobLevel'])
 
 #     Class of JobSatisfaction field should be FACTOR not INT
-empdata[,'JobSatisfaction']<-factor(empdata[,'JobSatisfaction'])
+empdata.raw[,'JobSatisfaction']<-factor(empdata.raw[,'JobSatisfaction'])
 
 #     Class of PerformanceRating field should be FACTOR not INT
-empdata[,'PerformanceRating']<-factor(empdata[,'PerformanceRating'])
+empdata.raw[,'PerformanceRating']<-factor(empdata.raw[,'PerformanceRating'])
 
 #     Class of RelationshipSatisfaction field should be FACTOR not INT
-empdata[,'RelationshipSatisfaction']<-factor(empdata[,'RelationshipSatisfaction'])
+empdata.raw[,'RelationshipSatisfaction']<-factor(empdata.raw[,'RelationshipSatisfaction'])
 
 #     Class of StockOptionLevel field should be FACTOR not INT
-empdata[,'StockOptionLevel']<-factor(empdata[,'StockOptionLevel'])
+empdata.raw[,'StockOptionLevel']<-factor(empdata.raw[,'StockOptionLevel'])
 
 #     class of WorkLifeBalance field should be FACTOR not INT
-empdata[,'WorkLifeBalance']<-factor(empdata[,'WorkLifeBalance'])
+empdata.raw[,'WorkLifeBalance']<-factor(empdata.raw[,'WorkLifeBalance'])
 
 # Verify the updated structure of empdata dataframe
-str(empdata)
+str(empdata.raw)
 
 # Verify that there is no data missing from the dataframe
-sum(is.na(empdata))
+sum(is.na(empdata.raw))
 
 # Shorten dataframe column names that exceed 12 characters
+#
+empdata.updcolnames <- empdata.raw
 # Note that the respective original column name is in comment below for reference if needed.
-colnames(empdata)[3] <- "BusinessTrvl"     # BusinessTravel
-colnames(empdata)[6] <- "DistFromHome"     # DistanceFromHome
-colnames(empdata)[8] <- "EducationFld"     # EducationField
-colnames(empdata)[9] <- "EmployeeCnt"      # EmployeeCount
-colnames(empdata)[10] <- "EmployeeNum"     # EmployeeNumber
-colnames(empdata)[11] <- "EnvirSatisf"     # EnvironmentSatisfaction
-colnames(empdata)[14] <- "JobInvolvmnt"    # JobInvolvement
-colnames(empdata)[17] <- "JobSatisf"       # JobSatisfaction
-colnames(empdata)[18] <- "MaritalStat"     # MaritalStatus
-colnames(empdata)[19] <- "MonthlyInc"      # MonthlyIncome
-colnames(empdata)[21] <- "NumFirmsWrkd"    # NumCompaniesWorked
-colnames(empdata)[24] <- "PctSalryHike"    # PercentSalaryHike
-colnames(empdata)[25] <- "PerforRating"    # PerformanceRating
-colnames(empdata)[26] <- "RelshipSatis"    # RelationshipSatisfaction
-colnames(empdata)[27] <- "StandardHrs"     # StandardHours
-colnames(empdata)[28] <- "StockOptLvl"     # StockOptionLevel
-colnames(empdata)[29] <- "TotlWrkngYrs"    # TotalWorkingYears
-colnames(empdata)[30] <- "TrnCntLastYr"    # TrainingTimesLastYear
-colnames(empdata)[31] <- "WorkLifeBal"     # WorkLifeBalance
-colnames(empdata)[32] <- "YrsAtFirm"       # YearsAtCompany
-colnames(empdata)[33] <- "YrsInCurRole"    # YearsInCurrentRole
-colnames(empdata)[34] <- "YrsSncePromo"    # YearsSinceLastPromotion
-colnames(empdata)[35] <- "YrsWthCurMgr"    # YearsWithCurrentManager
+#
+colnames(empdata.updcolnames)[3] <- "BusinessTrvl"     # BusinessTravel
+colnames(empdata.updcolnames)[6] <- "DistFromHome"     # DistanceFromHome
+colnames(empdata.updcolnames)[8] <- "EducationFld"     # EducationField
+colnames(empdata.updcolnames)[9] <- "EmployeeCnt"      # EmployeeCount
+colnames(empdata.updcolnames)[10] <- "EmployeeNum"     # EmployeeNumber
+colnames(empdata.updcolnames)[11] <- "EnvirSatisf"     # EnvironmentSatisfaction
+colnames(empdata.updcolnames)[14] <- "JobInvolvmnt"    # JobInvolvement
+colnames(empdata.updcolnames)[17] <- "JobSatisf"       # JobSatisfaction
+colnames(empdata.updcolnames)[18] <- "MaritalStat"     # MaritalStatus
+colnames(empdata.updcolnames)[19] <- "MonthlyInc"      # MonthlyIncome
+colnames(empdata.updcolnames)[21] <- "NumFirmsWrkd"    # NumCompaniesWorked
+colnames(empdata.updcolnames)[24] <- "PctSalryHike"    # PercentSalaryHike
+colnames(empdata.updcolnames)[25] <- "PerforRating"    # PerformanceRating
+colnames(empdata.updcolnames)[26] <- "RelshipSatis"    # RelationshipSatisfaction
+colnames(empdata.updcolnames)[27] <- "StandardHrs"     # StandardHours
+colnames(empdata.updcolnames)[28] <- "StockOptLvl"     # StockOptionLevel
+colnames(empdata.updcolnames)[29] <- "TotlWrkngYrs"    # TotalWorkingYears
+colnames(empdata.updcolnames)[30] <- "TrnCntLastYr"    # TrainingTimesLastYear
+colnames(empdata.updcolnames)[31] <- "WorkLifeBal"     # WorkLifeBalance
+colnames(empdata.updcolnames)[32] <- "YrsAtFirm"       # YearsAtCompany
+colnames(empdata.updcolnames)[33] <- "YrsInCurRole"    # YearsInCurrentRole
+colnames(empdata.updcolnames)[34] <- "YrsSncePromo"    # YearsSinceLastPromotion
+colnames(empdata.updcolnames)[35] <- "YrsWthCurMgr"    # YearsWithCurrentManager
 
+#
+empdata.okcolnames <- empdata.updcolnames
 #Verify column name updates to empdata dataframe
-str(empdata)
+str(empdata.okcolnames)
 
 # Define set of functions to set value of a new description column for each level and rating column
 #    Define function to set value of a new description column using the Education level list
@@ -122,24 +123,26 @@ set_low2outstanddescrip <- function (df, sourcecol, newcol){
 #Create and Populate new description fields for corresponding level and ratings fields 
 #Note that all new field names are no longer than 12 characters long.
 #    Create and Populate a new Education Description field
-empdata1 <- set_educdescrip(empdata, "Education", "EducDesc")
+empdata.educdesc <- set_educdescrip(empdata.okcolnames, "Education", "EducDesc")
 
 #    Create and Populate a new Environment Satisfaction Description field
-empdata2 <- set_low2veryhighdescrip(empdata1, "EnvirSatisf", "EnvirSatDesc")
+empdata.envirsatdesc <- set_low2veryhighdescrip(empdata.educdesc, "EnvirSatisf", "EnvirSatDesc")
 #    Create and Populate a new Job Involvement Description field
-empdata3 <- set_low2veryhighdescrip(empdata2, "JobInvolvmnt", "JobInvlvDesc")
+empdata.jobinvlvdesc <- set_low2veryhighdescrip(empdata.envirsatdesc, "JobInvolvmnt", "JobInvlvDesc")
 #    Create and Populate a new Job Satisfaction Description field
-empdata4 <- set_low2veryhighdescrip(empdata3, "JobSatisf", "JobSatDesc")
+empdata.jobsatdesc <- set_low2veryhighdescrip(empdata.jobinvlvdesc, "JobSatisf", "JobSatDesc")
 #    Create and Populate a new Relationship Satisfaction Description field
-empdata5 <- set_low2veryhighdescrip(empdata4, "RelshipSatis", "RlshpSatDesc")
+empdata.rlshpsatdesc <- set_low2veryhighdescrip(empdata.jobsatdesc, "RelshipSatis", "RlshpSatDesc")
 
 #    Create and Populate a new Performance Rating Description field
-empdata6 <- set_low2outstanddescrip(empdata5, "PerforRating", "PerfRtgDesc")
+empdata.perfrtgdesc <- set_low2outstanddescrip(empdata.rlshpsatdesc, "PerforRating", "PerfRtgDesc")
 
 #    Create and Populate a new WorkLifeBalance Description field
-empdatafinal <- set_bad2bestdescrip(empdata6, "WorkLifeBal", "WkLifBalDesc")
+empdata.wklifbaldesc <- set_bad2bestdescrip(empdata.perfrtgdesc, "WorkLifeBal", "WkLifBalDesc")
 
+#
+empdata.tidy <- empdata.wklifbaldesc
 #Verify structure of final dataframe
-str(empdatafinal)
+str(empdata.tidy)
 
 #TEAM QUESTION:   do we need to convert the new level and rating Description fields to factors??
