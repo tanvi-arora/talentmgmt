85cf51f737908b19dd3179ed9aefdabbc6c4fa86
Anjli Solsi <asolsi@smu.edu>
2018-08-06 18:20:59 -0400

    update data visualization scripts

--- R/age_inc_analysis.R
+++ R/age_inc_analysis.R
@@ -7,17 +7,28 @@
 ###########################################################################################
 ##@knitr age_inc_analysis
 
+#first bar plot representation of Total Monthly Income by Age
+options(scipen = 999)
+brks <- seq(-500000, 500000, 50000)
+lbls <- paste0(as.character(c(seq(500, 0, -50), seq(50, 500, 50))), "k")
+ggplot(empdata.tidy, aes(x=reorder(Age, Age), y=MonthlyInc, fill=Gender)) + 
+  geom_bar(stat="identity", width=.7, position="stack") + scale_y_continuous(breaks=brks, labels=lbls) +
+  coord_flip() + labs(title="Total Monthly Income by Age", x="Age", y="Total Monthly Income (Dollars)") + 
+  theme_tufte() + theme(plot.title = element_text(hjust=.5), axis.ticks = element_blank()) +
+  scale_fill_brewer(palette = "Set1")
 
-#bar plot of Monthly Income by Age
-ggplot(empdata.tidy, aes(x=reorder(Age, -Age), y=MonthlyInc, fill=Gender)) + geom_bar(color="black", stat="identity") + 
-  scale_fill_brewer(palette="Set1") + theme_gray()+ labs(title="Monthly Employee Income by Age", x="Age of Employee", y="Monthly Income (Dollars)") + 
+#second bar plot representation of Total Monthly Income by Age
+options(scipen = 999)
+ggplot(empdata.tidy, aes(x=reorder(Age, Age), y=MonthlyInc, fill=Gender)) + 
+  geom_bar(stat="identity", position = "stack", size =.3) + 
+  scale_fill_brewer(palette="Set1") + theme_gray() + 
+  labs(title="Total Monthly Employee Income by Age", x="Age of Employee", y="Total Monthly Income (Dollars)") + 
   theme(plot.title = element_text(hjust=0.5)) + theme(axis.text.x = element_text(angle = 90, hjust = 1)) 
 
-#scatterplot of Monthly Income by Age
+#scatterplot of Total Monthly Income by Age with a linear model fit line and 99% confidence level interval
 ggplot(empdata.tidy, aes(Age, MonthlyInc)) + geom_point(aes(shape=Gender, colour = Gender)) + theme_bw() +
   geom_smooth(method=lm, linetype="dashed", color="yellow", level=0.99) + 
   labs(title="Monthly Employee Income by Age", x="Age of Employee", y="Monthly Income (Dollars)") + 
   theme(plot.title = element_text(hjust=0.5)) 
-
 
 
---
+++ R/life_sat_analysis.R
@@ -0,0 +1,78 @@
+#####################################################################################################
+# Script name : life_sat_analysis.R 
+# Created By  : Anjli Solsi
+# Created Dt  : 08/4/2018
+# Description : R code to analyze the relationship between variables related to Work Life Satisfaction
+# Source File : This script uses the cleaned data from tidy_dataset.R (empdata.tidy)
+#####################################################################################################
+##@knitr life_sat_analysis
+
+#examine linear regression model of the data with variables attributed to work life satisfaction
+trial <- lm((formula = as.numeric(WorkLifeBal) ~ EnvirSatisf + JobInvolvmnt + JobSatisf + PerforRating + RelshipSatis), data = empdata.tidy)
+plot(trial)
+
+#get percentages for each rating of Work Life Balance
+empdata.WorkLifeBal <- as.data.frame(empdata.tidy %>% group_by(WorkLifeBal) %>% summarise( n=n()) %>% mutate(percent = round(n*100/sum(n),2)))
+empdata.WorkLifeBal
+
+#get percentages for each rating of Environment Satisfaction
+empdata.EnvirSatisf <- as.data.frame(empdata.tidy %>% group_by(EnvirSatisf) %>% summarise( n=n()) %>% mutate(percent = round(n*100/sum(n),2)))
+empdata.EnvirSatisf
+
+#get percentages for each rating of Job Involvement
+empdata.JobInvolvmnt <- as.data.frame(empdata.tidy %>% group_by(JobInvolvmnt) %>% summarise( n=n()) %>% mutate(percent = round(n*100/sum(n),2)))
+empdata.JobInvolvmnt
+
+#get percentages for each rating of Job Satisfaction
+empdata.JobSatisf <- as.data.frame(empdata.tidy %>% group_by(JobSatisf) %>% summarise( n=n()) %>% mutate(percent = round(n*100/sum(n),2)))
+empdata.JobSatisf
+
+#get percentages for each rating of Performance Rating
+empdata.PerforRating <- as.data.frame(empdata.tidy %>% group_by(PerforRating) %>% summarise( n=n()) %>% mutate(percent = round(n*100/sum(n),2)))
+empdata.PerforRating
+
+#get percentages for each rating of Work Relationship Satisfaction
+empdata.RelshipSatis <- as.data.frame(empdata.tidy %>% group_by(RelshipSatis) %>% summarise( n=n()) %>% mutate(percent = round(n*100/sum(n),2)))
+empdata.RelshipSatis
+
+#bar plot showing ratings of Work Life Balance and percentage of employees for each rating
+plot1 <- ggplot(data = empdata.WorkLifeBal, aes(x = empdata.WorkLifeBal$WorkLifeBal, y = empdata.WorkLifeBal$percent, fill = empdata.WorkLifeBal$percent)) + 
+  geom_bar(stat="identity") + scale_fill_gradient(low = "#FFCC99", high = "#990000") +
+  labs(title="Work Life Balance") + xlab("Rating") + ylab("Percentage of Employees")  + 
+  theme(plot.title = element_text(hjust=0.5), panel.background = element_blank())
+
+#bar plot showing ratings of Environment Satisfaction and percentage of employees for each rating
+plot2 <- ggplot(data = empdata.EnvirSatisf, aes(x = empdata.EnvirSatisf$EnvirSatisf, y = empdata.EnvirSatisf$percent, fill = empdata.EnvirSatisf$percent)) + 
+  geom_bar(stat="identity") + scale_fill_gradient(low = "#FFCC99", high = "#990000") +
+  labs( title="Environment Satisfaction") + xlab("Rating") + ylab("Percentage of Employees")  + 
+  theme( plot.title = element_text(hjust=0.5), panel.background = element_blank())
+
+#bar plot showing ratings of Job Involvement and percentage of employees for each rating
+plot3 <- ggplot(data = empdata.JobInvolvmnt, aes(x = empdata.JobInvolvmnt$JobInvolvmnt, y = empdata.JobInvolvmnt$percent, fill = empdata.JobInvolvmnt$percent)) + 
+  geom_bar(stat="identity") + scale_fill_gradient(low = "#FFCC99", high = "#990000") +
+  labs( title="Job Involvement") + xlab("Rating") + ylab("Percentage of Employees")  + 
+  theme( plot.title = element_text(hjust=0.5), panel.background = element_blank()) 
+
+#bar plot showing ratings of Job Satisfaction and percentage of employees for each rating
+plot4 <- ggplot(data = empdata.JobSatisf, aes(x = empdata.JobSatisf$JobSatisf, y = empdata.JobSatisf$percent, fill = empdata.JobSatisf$percent)) + 
+  geom_bar(stat="identity") + scale_fill_gradient(low = "#FFCC99", high = "#990000") +
+  labs( title="Job Satisfaction") + xlab("Rating") + ylab("Percentage of Employees")  + 
+  theme( plot.title = element_text(hjust=0.5), panel.background = element_blank()) 
+
+#bar plot showing ratings of Performance Rating and percentage of employees for each rating
+plot5 <- ggplot(data = empdata.PerforRating, aes(x = empdata.PerforRating$PerforRating, y = empdata.PerforRating$percent, fill = empdata.PerforRating$percent)) + 
+  geom_bar(stat="identity") + scale_fill_gradient(low = "#FFCC99", high = "#990000") +
+  labs( title="Performance Rating") + xlab("Rating") + ylab("Percentage of Employees")  + 
+  theme( plot.title = element_text(hjust=0.5), panel.background = element_blank()) 
+
+#bar plot showing ratings of Work Relationship Satisfaction and percentage of employees for each rating
+plot6 <- ggplot(data = empdata.RelshipSatis, aes(x = empdata.RelshipSatis$RelshipSatis, y = empdata.RelshipSatis$percent, fill = empdata.RelshipSatis$percent)) + 
+  geom_bar(stat="identity") + scale_fill_gradient(low = "#FFCC99", high = "#990000") +
+  labs( title="Relationship Satisfaction") + xlab("Rating") + ylab("Percentage of Employees")  + 
+  theme( plot.title = element_text(hjust=0.5), panel.background = element_blank()) 
+
+#display all 6 plots together 
+grid.arrange(plot1, plot2, plot3, plot4, plot5, plot6, ncol=2, nrow=3)
+
+
+
