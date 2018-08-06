77435ba2d405d7b04eea3d63690c493ff1465ac2
Anjli Solsi <asolsi@smu.edu>
2018-08-06 18:27:27 -0400

    update life satisfaction plots

--- R/life_sat_analysis.R
+++ R/life_sat_analysis.R
@@ -37,38 +37,38 @@ empdata.RelshipSatis
 
 #bar plot showing ratings of Work Life Balance and percentage of employees for each rating
 plot1 <- ggplot(data = empdata.WorkLifeBal, aes(x = empdata.WorkLifeBal$WorkLifeBal, y = empdata.WorkLifeBal$percent, fill = empdata.WorkLifeBal$percent)) + 
-  geom_bar(stat="identity") + scale_fill_gradient(low = "#FFCC99", high = "#990000") +
-  labs(title="Work Life Balance") + xlab("Rating") + ylab("Percentage of Employees")  + 
+  geom_bar(stat="identity") + scale_fill_gradient(low = "#FFCC99", high = "#990000") + guides(fill=FALSE) +
+  labs(title="Work Life Balance") + xlab("Rating") + ylab("% of Employees")  + 
   theme(plot.title = element_text(hjust=0.5), panel.background = element_blank())
 
 #bar plot showing ratings of Environment Satisfaction and percentage of employees for each rating
 plot2 <- ggplot(data = empdata.EnvirSatisf, aes(x = empdata.EnvirSatisf$EnvirSatisf, y = empdata.EnvirSatisf$percent, fill = empdata.EnvirSatisf$percent)) + 
-  geom_bar(stat="identity") + scale_fill_gradient(low = "#FFCC99", high = "#990000") +
-  labs( title="Environment Satisfaction") + xlab("Rating") + ylab("Percentage of Employees")  + 
+  geom_bar(stat="identity") + scale_fill_gradient(low = "#FFCC99", high = "#990000") + guides(fill=FALSE) +
+  labs( title="Environment Satisfaction") + xlab("Rating") + ylab("% of Employees")  + 
   theme( plot.title = element_text(hjust=0.5), panel.background = element_blank())
 
 #bar plot showing ratings of Job Involvement and percentage of employees for each rating
 plot3 <- ggplot(data = empdata.JobInvolvmnt, aes(x = empdata.JobInvolvmnt$JobInvolvmnt, y = empdata.JobInvolvmnt$percent, fill = empdata.JobInvolvmnt$percent)) + 
-  geom_bar(stat="identity") + scale_fill_gradient(low = "#FFCC99", high = "#990000") +
-  labs( title="Job Involvement") + xlab("Rating") + ylab("Percentage of Employees")  + 
+  geom_bar(stat="identity") + scale_fill_gradient(low = "#FFCC99", high = "#990000") + guides(fill=FALSE) +
+  labs( title="Job Involvement") + xlab("Rating") + ylab("% of Employees")  + 
   theme( plot.title = element_text(hjust=0.5), panel.background = element_blank()) 
 
 #bar plot showing ratings of Job Satisfaction and percentage of employees for each rating
 plot4 <- ggplot(data = empdata.JobSatisf, aes(x = empdata.JobSatisf$JobSatisf, y = empdata.JobSatisf$percent, fill = empdata.JobSatisf$percent)) + 
-  geom_bar(stat="identity") + scale_fill_gradient(low = "#FFCC99", high = "#990000") +
-  labs( title="Job Satisfaction") + xlab("Rating") + ylab("Percentage of Employees")  + 
+  geom_bar(stat="identity") + scale_fill_gradient(low = "#FFCC99", high = "#990000") + guides(fill=FALSE) +
+  labs( title="Job Satisfaction") + xlab("Rating") + ylab("% of Employees")  + 
   theme( plot.title = element_text(hjust=0.5), panel.background = element_blank()) 
 
 #bar plot showing ratings of Performance Rating and percentage of employees for each rating
 plot5 <- ggplot(data = empdata.PerforRating, aes(x = empdata.PerforRating$PerforRating, y = empdata.PerforRating$percent, fill = empdata.PerforRating$percent)) + 
-  geom_bar(stat="identity") + scale_fill_gradient(low = "#FFCC99", high = "#990000") +
-  labs( title="Performance Rating") + xlab("Rating") + ylab("Percentage of Employees")  + 
+  geom_bar(stat="identity") + scale_fill_gradient(low = "#FFCC99", high = "#990000") + guides(fill=FALSE) +
+  labs( title="Performance Rating") + xlab("Rating") + ylab("% of Employees")  + 
   theme( plot.title = element_text(hjust=0.5), panel.background = element_blank()) 
 
 #bar plot showing ratings of Work Relationship Satisfaction and percentage of employees for each rating
 plot6 <- ggplot(data = empdata.RelshipSatis, aes(x = empdata.RelshipSatis$RelshipSatis, y = empdata.RelshipSatis$percent, fill = empdata.RelshipSatis$percent)) + 
-  geom_bar(stat="identity") + scale_fill_gradient(low = "#FFCC99", high = "#990000") +
-  labs( title="Relationship Satisfaction") + xlab("Rating") + ylab("Percentage of Employees")  + 
+  geom_bar(stat="identity") + scale_fill_gradient(low = "#FFCC99", high = "#990000") + guides(fill=FALSE) +
+  labs( title="Relationship Satisfaction") + xlab("Rating") + ylab("% of Employees")  + 
   theme( plot.title = element_text(hjust=0.5), panel.background = element_blank()) 
 
 #display all 6 plots together 
